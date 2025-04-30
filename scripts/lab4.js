07bdf79778c95da59d1aa71cf55fe26991dacc8b
Chanbin Na <chanbinna@gmail.com>
2025-04-29 21:50:00 -0700

    fixed extra bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,6 +7,10 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            return false;
+        }
+
         let result = 0;
 
         result = num1 + num2;
@@ -14,7 +18,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -25,6 +29,10 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || prices.length === 0 || discount < 0 || discount > 1) {
+        return false;
+    }
+
     const discounted = []
     const length = prices.length;
     for (let i = 0; i < length; i++) {
